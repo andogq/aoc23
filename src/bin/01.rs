1e2d434fb5a334bd37e63140308a7ff20189f62a
Tom Anderson <tom@ando.gq>
2023-12-01 11:47:50 +0100

    Clean up and speed up day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,32 +1,30 @@
 advent_of_code::solution!(1);
 
-fn find_calibration_value<S: AsRef<str>>(lines: impl IntoIterator<Item = S>) -> u32 {
-    lines
-        .into_iter()
-        .flat_map(|line| {
-            let line = line.as_ref().chars();
-
-            let first = line
-                .clone()
-                .find(|c| c.is_numeric())
-                .and_then(|first| first.to_digit(10));
-            let last = line
-                .rev()
-                .find(|c| c.is_numeric())
-                .and_then(|first| first.to_digit(10));
-
-            if let (Some(first), Some(last)) = (first, last) {
-                Some((first, last))
-            } else {
-                None
-            }
-        })
-        .map(|(tens, ones)| (tens * 10) + ones)
-        .sum()
-}
-
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(find_calibration_value(input.lines()))
+    Some(
+        input
+            .lines()
+            .flat_map(|line| {
+                let line = line.chars();
+
+                let first = line
+                    .clone()
+                    .find(|c| c.is_numeric())
+                    .and_then(|first| first.to_digit(10));
+                let last = line
+                    .rev()
+                    .find(|c| c.is_numeric())
+                    .and_then(|last| last.to_digit(10));
+
+                if let (Some(first), Some(last)) = (first, last) {
+                    Some((first, last))
+                } else {
+                    None
+                }
+            })
+            .map(|(tens, ones)| (tens * 10) + ones)
+            .sum(),
+    )
 }
 
 const DIGITS: [&str; 9] = [
@@ -34,45 +32,63 @@ const DIGITS: [&str; 9] = [
 ];
 
 pub fn part_two(input: &str) -> Option<u32> {
-    fn match_line(line: &str) -> String {
-        let mut matches = DIGITS
-            .into_iter()
-            .enumerate()
-            .flat_map(|(value, digit)| {
-                (0..line.len())
-                    .filter(move |&i| line[i..].starts_with(digit))
-                    .map(move |index| (index, (value + 1).to_string()))
+    let digit_values = DIGITS
+        .into_iter()
+        .enumerate()
+        .map(|(value, digit)| (value as u32 + 1, digit.to_string()))
+        .collect::<Vec<_>>();
+    let digit_rev_values = DIGITS
+        .into_iter()
+        .enumerate()
+        .map(|(value, digit)| (value as u32 + 1, digit.chars().rev().collect::<String>()))
+        .collect::<Vec<_>>();
+
+    Some(
+        input
+            .lines()
+            .flat_map(|line| {
+                let line_rev = line.chars().rev().collect::<String>();
+
+                let mut iter = [
+                    (line.to_string(), &digit_values),
+                    (line_rev, &digit_rev_values),
+                ]
+                .into_iter()
+                .flat_map(|(line, digit_values)| {
+                    let digit = line
+                        .chars()
+                        .enumerate()
+                        .find(|(_, c)| c.is_numeric())
+                        .and_then(|(index, c)| c.to_digit(10).map(|d| (index, d)));
+
+                    let word = digit_values
+                        .iter()
+                        .flat_map(|(value, d)| {
+                            line[..digit.map(|(index, _)| index).unwrap_or(line.len())]
+                                .find(d)
+                                .map(|index| (index, *value))
+                        })
+                        .min_by_key(|(index, _)| *index);
+
+                    match (word, digit) {
+                        (Some((word_index, word_value)), Some((digit_index, digit_value))) => {
+                            Some(if word_index < digit_index {
+                                word_value
+                            } else {
+                                digit_value
+                            })
+                        }
+                        (Some((_, value)), None) => Some(value),
+                        (None, Some((_, value))) => Some(value),
+                        (None, None) => None,
+                    }
+                })
+                .take(2);
+
+                Some(iter.next()? * 10 + iter.next()?)
             })
-            .collect::<Vec<_>>();
-
-        matches.sort_unstable_by_key(|(index, _)| index.clone());
-
-        matches
-            .into_iter()
-            .map(|(_, digit)| digit)
-            .collect::<String>()
-    }
-
-    Some(find_calibration_value(input.lines().map(|line| {
-        let chars = line.chars();
-
-        let (line, current_word) = chars.clone().fold(
-            ("".to_string(), "".to_string()),
-            |(mut line, mut current_word), c| {
-                if c.is_numeric() {
-                    line += &match_line(&current_word);
-                    line.push(c);
-                    current_word = "".to_string();
-                } else {
-                    current_word.push(c);
-                }
-
-                (line, current_word)
-            },
-        );
-
-        line + &match_line(&current_word)
-    })))
+            .sum(),
+    )
 }
 
 #[cfg(test)]
