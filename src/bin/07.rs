405c541c60c211c998c33bb596412cb3dca8ffba
Tom Anderson <tom@ando.gq>
2023-12-07 12:12:02 +0100

    Cleaning for day 7

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -12,17 +12,57 @@ enum HandType {
     FiveOfAKind,
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut hands = input
+impl From<&[u32; 5]> for HandType {
+    fn from(hand: &[u32; 5]) -> Self {
+        // Count all non-joker cards
+        let counts =
+            hand.iter()
+                .filter(|&&c| c != JOKER_VALUE)
+                .fold([0u32; 15], |mut counts, &card| {
+                    counts[card as usize] += 1;
+                    counts
+                });
+
+        let (max_count, next_count) = counts.into_iter().fold((0, 0), |(max, max_2), count| {
+            if count >= max {
+                (count, max)
+            } else if count >= max_2 {
+                (max, count)
+            } else {
+                (max, max_2)
+            }
+        });
+
+        let joker_count = hand.iter().filter(|&&c| c == JOKER_VALUE).count() as u32;
+
+        match (max_count + joker_count, next_count) {
+            (5, _) => HandType::FiveOfAKind,
+            (4, _) => HandType::FourOfAKind,
+            (3, 2) => HandType::FullHouse,
+            (3, _) => HandType::ThreeOfAKind,
+            (2, 3) => HandType::FullHouse,
+            (2, 2) => HandType::TwoPair,
+            (2, _) => HandType::OnePair,
+            (1, _) => HandType::HighCard,
+            _ => unreachable!(),
+        }
+    }
+}
+
+const JACK_VALUE: u32 = 11;
+const JOKER_VALUE: u32 = 1;
+
+fn parse_hands(input: &str, j_value: u32) -> impl Iterator<Item = ([u32; 5], u32)> + '_ {
+    input
         .lines()
         .flat_map(|line| line.split_once(" "))
-        .flat_map(|(hand, bid)| {
+        .flat_map(move |(hand, bid)| {
             Some::<([u32; 5], _)>((
                 hand.chars()
                     .flat_map(|card| match card {
                         '2'..='9' => card.to_digit(10),
                         'T' => Some(10),
-                        'J' => Some(11),
+                        'J' => Some(j_value),
                         'Q' => Some(12),
                         'K' => Some(13),
                         'A' => Some(14),
@@ -34,137 +74,28 @@ pub fn part_one(input: &str) -> Option<u32> {
                 bid.parse::<u32>().ok()?,
             ))
         })
-        .map(|(hand, bid)| {
-            (
-                {
-                    let mut sorted_hand = hand.clone();
-                    sorted_hand.sort_unstable();
-                    let mut sorted_hand = sorted_hand.into_iter().peekable();
+}
 
-                    let mut current_guess = HandType::HighCard;
-
-                    while let Some(card) = sorted_hand.next() {
-                        let mut same = 1;
-                        while sorted_hand.peek().map(|&c| c == card).unwrap_or_default() {
-                            sorted_hand.next();
-                            same += 1;
-                        }
-
-                        current_guess = match same {
-                            5 => HandType::FiveOfAKind,
-                            4 => HandType::FourOfAKind,
-                            3 if matches!(current_guess, HandType::OnePair) => HandType::FullHouse,
-                            3 => HandType::ThreeOfAKind,
-                            2 if matches!(current_guess, HandType::OnePair) => HandType::TwoPair,
-                            2 if matches!(current_guess, HandType::ThreeOfAKind) => {
-                                HandType::FullHouse
-                            }
-                            2 => HandType::OnePair,
-                            _ => current_guess,
-                        };
-                    }
-
-                    current_guess
-                },
-                hand,
-                bid,
-            )
-        })
+fn run_game(hands: impl Iterator<Item = ([u32; 5], u32)>) -> u32 {
+    let mut hands = hands
+        .map(|(hand, bid)| (HandType::from(&hand), hand, bid))
         .collect::<Vec<_>>();
 
     hands.sort_unstable_by_key(|(ty, hand, _)| (*ty as u32, hand.clone()));
 
-    Some(
-        hands
-            .into_iter()
-            .enumerate()
-            .map(|(rank, (_, _, bid))| (rank as u32 + 1) * bid)
-            .sum(),
-    )
+    hands
+        .into_iter()
+        .enumerate()
+        .map(|(rank, (_, _, bid))| (rank as u32 + 1) * bid)
+        .sum()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(run_game(parse_hands(input, JACK_VALUE)))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let mut hands = input
-        .lines()
-        .flat_map(|line| line.split_once(" "))
-        .flat_map(|(hand, bid)| {
-            Some::<([u32; 5], _)>((
-                hand.chars()
-                    .flat_map(|card| match card {
-                        'J' => Some(1),
-                        '2'..='9' => card.to_digit(10),
-                        'T' => Some(10),
-                        'Q' => Some(11),
-                        'K' => Some(12),
-                        'A' => Some(13),
-                        _ => None,
-                    })
-                    .collect::<Vec<_>>()
-                    .try_into()
-                    .ok()?,
-                bid.parse::<u32>().ok()?,
-            ))
-        })
-        .flat_map(|(hand, bid)| {
-            Some((
-                {
-                    let mut sorted_hand = hand
-                        .iter()
-                        .filter(|&&c| c != 1)
-                        .cloned()
-                        .collect::<Vec<_>>();
-                    sorted_hand.sort_unstable();
-
-                    let joker_count = hand.len() - sorted_hand.len();
-
-                    // Find the most popular card in the hand
-                    let mut card_counts = Vec::new();
-
-                    let mut hand_iter = sorted_hand.iter().peekable();
-                    while let Some(card) = hand_iter.next() {
-                        let mut count = 1;
-                        while hand_iter.peek().map(|&c| c == card).unwrap_or_default() {
-                            hand_iter.next();
-                            count += 1;
-                        }
-
-                        card_counts.push(count);
-                    }
-
-                    card_counts.sort_unstable();
-                    card_counts.reverse();
-                    let mut iter = card_counts.into_iter();
-
-                    let max_count = iter.next().unwrap_or_default() + joker_count;
-                    let next_count = iter.next();
-
-                    match (max_count, next_count) {
-                        (5, None) => HandType::FiveOfAKind,
-                        (4, _) => HandType::FourOfAKind,
-                        (3, Some(2)) => HandType::FullHouse,
-                        (3, _) => HandType::ThreeOfAKind,
-                        (2, Some(3)) => HandType::FullHouse,
-                        (2, Some(2)) => HandType::TwoPair,
-                        (2, _) => HandType::OnePair,
-                        (1, _) => HandType::HighCard,
-                        _ => unreachable!(),
-                    }
-                },
-                hand,
-                bid,
-            ))
-        })
-        .collect::<Vec<_>>();
-
-    hands.sort_unstable_by_key(|(ty, hand, _)| (*ty as u32, hand.clone()));
-
-    Some(
-        hands
-            .into_iter()
-            .enumerate()
-            .map(|(rank, (_, _, bid))| (rank as u32 + 1) * bid)
-            .sum(),
-    )
+    Some(run_game(parse_hands(input, JOKER_VALUE)))
 }
 
 #[cfg(test)]
